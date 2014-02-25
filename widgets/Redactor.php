293684bfa644788bb35d471168441f5623dea097
sim2github <sim.on@ua.fm>
2014-02-25 02:50:19 +0200

    Update Redactor.php

--- widgets/Redactor.php
+++ widgets/Redactor.php
@@ -5,7 +5,7 @@
  * @license http://www.yiiframework.com/license/
  */
 
-namespace app\modules\redactor\widgets;
+namespace sim2github\imperavi\widgets;
 use Yii;
 use yii\widgets\InputWidget;
 use yii\helpers\Html;
