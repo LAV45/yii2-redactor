ef22b2c97f98a0b9f3d4131d493fbb6324aa3f9d
sim2github <sim.on@ua.fm>
2014-07-09 01:54:10 +0300

    Ace Plugin WIP

--- assets/plugins/ace/ace.js
+++ assets/plugins/ace/ace.js
@@ -7,12 +7,45 @@ if (typeof RedactorPlugins === 'undefined') var RedactorPlugins = {};
 RedactorPlugins.ace = {
 	init: function()
 	{
-		this.buttonRemove('html');
-		this.buttonAddFirst('ace', 'Ace editor', this.initAce);
-		$('.re-ace').removeClass('re-ace').addClass('re-html');
-	},
-	initAce: function()
-	{
+		var $redactor = this;
 
+		this.buttonRemove('html');
+		this.buttonAddFirst('ace', 'Ace editor');
+		this.buttonGet('ace')
+			.addClass('re-html')
+			.click(
+				function(){
+					if ($redactor.opts.visual) {
+						$redactor.toggleCode(false);
+
+								$redactor.$source
+									.before('<div id="ace-editor" style="width:'+$redactor.$editor.outerWidth()+'px;height:'+$redactor.$editor.outerHeight()+'px;"></div>')
+									.hide();
+
+								var editor = ace.edit("ace-editor");
+								// Settings
+								editor.getSession().setMode("ace/mode/html");
+								editor.setTheme("ace/theme/monokai");
+								editor.getSession().setUseSoftTabs(true);
+								editor.getSession().setUseWrapMode(true);
+								editor.setShowPrintMargin(false);
+								editor.setAutoScrollEditorIntoView(true);
+								editor.setOption("maxLines", 30);
+								editor.setOption("enableEmmet", true);
+
+								editor.getSession().setValue($redactor.$source.val());
+								editor.getSession().on('change', function() {
+									$redactor.$source.val(editor.getSession().getValue());
+								});
+
+					} else {
+						$redactor.toggleVisual();
+						$redactor.$box.find('#ace-editor').remove();
+
+
+					}
+
+				}
+			);
 	}
 }
